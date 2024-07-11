015b5651d6cbb2149f723cab325915408fa91873
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-07-11 20:00:31 -0300

    fixes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AuthLayout({ children}: { children: React.ReactNode;}) {
   return (
     <div className="grid lg:grid-cols-2 h-full items-center justify-center">
       <div className="flex items-center justify-center">{children}</div>
