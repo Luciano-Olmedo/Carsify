03b66893064f8b87286dd3841a93176f55caa10a
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-07-16 19:02:59 -0300

    fixes and separator ui

--- app/(routes)/(dashboard)/dashboard/components/Logo/LogoDashBoard.tsx
+++ app/(routes)/(dashboard)/dashboard/components/Logo/LogoDashBoard.tsx
@@ -4,9 +4,13 @@ import Image from "next/image";
 
 export  function LogoDashBoard() {
   return (
-    <Link href="/">
+    <Link href="/" className="flex items-center h-20 gap-2 border-b cursor-pointer min-h-20">
         <Image src="/logo.svg" alt="logo" height={30} width={30} priority/>
-        
+        <h1 className="text-xl font-bold">Carsify</h1>
+                                        
+
+
+
        
     </Link>
   )
--- app/(routes)/(dashboard)/dashboard/components/Sidebar/Sidebar.tsx
+++ app/(routes)/(dashboard)/dashboard/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { LogoDashBoard } from '../Logo'
-import { SidebarRoutes } from '../SidebarRoutes'
+import { SidebarRoutes } from '../SidebarRoutes/SidebarRoutes'
+
 
 export  function Sidebar() {
   return (
     <div className='h-screen'>
-        <div className='flex flex-col h-full border-r'>
+        <div className='flex flex-col h-full border-r px-6'>
           <LogoDashBoard/>
           <SidebarRoutes/>
         </div>
