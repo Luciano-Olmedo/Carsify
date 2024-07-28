7c720c5d51f79f8760d45bf846fe93c59e06f3e2
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-07-28 10:32:23 -0300

    adding more components from shadcn

---
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/FormAddCar/FormAddCar.tsx
@@ -0,0 +1,15 @@
+
+"use client"
+
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+export  function FormAddCar() {
+  return (
+    <div>FormAddCar</div>
+  )
+}
+
--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { ButtonAddCart } from './components/ButtonAddCar'
 
 export default function CarsManagerPage() {
   return (
-    <div>From cars manager page</div>
+    <div>
+        <div className='flex justify-between'>
+            <h2 className='text-2xl font-bold'>Manage your cars</h2>
+            <ButtonAddCart/>
+
+        </div>
+    </div>
   )
 }
