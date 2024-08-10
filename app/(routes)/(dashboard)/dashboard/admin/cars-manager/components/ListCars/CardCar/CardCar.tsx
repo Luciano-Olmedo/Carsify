c91285c927435c306da11a1010e20b7f7e925f18
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-08-09 21:11:06 -0300

    next config

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/CardCar.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/CardCar.tsx
@@ -1,10 +1,26 @@
-
+"use client"
+import { Button } from "@/components/ui/button";
+import { Toast } from "@/components/ui/toast";
+import { Fuel,Gauge,Trash, Upload,Users,Wrench,Gem } from "lucide-react";
 import { CardCarProps } from "./CardCar.type";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 
 export  function CardCar(props:CardCarProps) {
     const {car} = props;
   return (
-    <div>{car.name}</div>
+    <div className="relative p-1 bg-white rounded-lg shadow-md hover:shadow-lg">
+        <Image 
+          src={car.photo}
+          alt={car.name}
+          width={400}
+          height={600}
+          className="rounded-lg"
+        >
+
+        </Image>
+
+    </div>
   )
 }
