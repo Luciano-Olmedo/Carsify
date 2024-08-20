309624ec87e535fc803bd7f3437bb3dd75cfbf3b
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-08-19 21:09:39 -0300

    edit car

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/FormEditCar/FormEditCar.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/FormEditCar/FormEditCar.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import { FormEditCarProps } from "./FormEditCar.types";
-"use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
@@ -26,32 +28,213 @@ import {
 import { UploadButton } from "@/utils/uploadthing";
 import { useState } from "react";
 import { toast } from "@/components/ui/use-toast";
-import axios from "axios"
+import axios from "axios";
 import { useRouter } from "next/navigation";
 
-
-export  function FormEditCar(props:FormEditCarProps) {
- 
+export function FormEditCar(props: FormEditCarProps) {
+  const { carData, setOpenDialog } = props;
   const [photoUploaded, setPhotoUploaded] = useState(false);
-  const router = useRouter()
-  const {carData,setOpenDialog}= props;
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
-      cv: "",
-      transmission: "",
-      people: "",
-      photo: "",
-      engine: "",
-      type: "",
-      isPublish: false,
+      name: carData.name,
+      cv: carData.cv,
+      transmission: carData.transmission,
+      people: carData.people,
+      photo: carData.photo,
+      engine: carData.engine,
+      type: carData.type,
+      isPublish: carData.isPublish ? carData.isPublish : false,
     },
-    
-  })
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>)=>{
+    console.log("Submit")
+  }
+
   const { isValid } = form.formState;
   return (
-    <div>FormEditCar</div>
-  )
-  
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="grid gap-6 lg:grid-cols-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Toyota Corolla" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="cv"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Power</FormLabel>
+                <FormControl>
+                  <Input placeholder="150cv" type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="transmission"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Transmission</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select of type of car" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="manual">Manual</SelectItem>
+                    <SelectItem value="automatic">Automátic</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="people"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>People</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select the quantity of people" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="2">2</SelectItem>
+                    <SelectItem value="4">4</SelectItem>
+                    <SelectItem value="6">6</SelectItem>
+                    <SelectItem value="8">8</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="engine"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Engine</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select the engine of the car" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="gasoil">Gasolin</SelectItem>
+                    <SelectItem value="diesel">Diesel</SelectItem>
+                    <SelectItem value="electric">Electric</SelectItem>
+                    <SelectItem value="hybrid">Hybrid</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Type</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select the engine of the car" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="sedan">Sedán</SelectItem>
+                    <SelectItem value="suv">SUV</SelectItem>
+                    <SelectItem value="coupe">Coupé</SelectItem>
+                    <SelectItem value="familiar">Familiar</SelectItem>
+                    <SelectItem value="luxe">De luxe</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="photo"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Car Image</FormLabel>
+                <FormControl>
+                  {photoUploaded ? (
+                    <p>Image Uploaded !</p>
+                  ) : (
+                    <UploadButton
+                      className="rounded-lg bg-slate-600/20 text-slate-800 outline"
+                      {...field}
+                      endpoint="photo"
+                      onClientUploadComplete={(res) => {
+                        form.setValue("photo", res?.[0].url);
+                        setPhotoUploaded(true);
+                      }}
+                      onUploadError={(error: Error) => {
+                        console.log(error);
+                      }}
+                    />
+                  )}
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="priceDay"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price per day</FormLabel>
+                <FormControl>
+                  <Input placeholder="$5000" type="number" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit" className="w-full mt-5" disabled={!isValid}>
+          Modificar Cambios
+        </Button>
+      </form>
+    </Form>
+  );
 }
